e3f81bfecfbef52648b49e385a657fd289ef0759
Hatem <hmelshazly@gmail.com>
2015-01-14 10:49:21 -0400

    prepared workspace

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AccProApp::Application.config.secret_token = '6e4446898af6a793ec825e4270eb6900ac3f07537ee2b8e1ac57300256a29bc07e563b333067b72f6fcf1a92fbf015ddde607695fdb8c83bcc6cdbcf2e03b389'
