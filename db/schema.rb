f7fc86f913a6588c438e1dfe8a80a20c04309092
karim magdy <karim.magdy15@yahoo.com>
2015-01-17 10:58:36 +0200

    Creating models and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20150117085156) do
+
+  create_table "accounts", :force => true do |t|
+    t.string   "name"
+    t.float    "balance"
+    t.string   "type"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
